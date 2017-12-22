bdb693272f61e8b13d718b80006e90725c8d7dc0
Jessica Swift <jessica.nicole.swift@gmail.com>
2017-12-22 01:21:14 -0600

    display corresponding colors with tshirt design

--- js/interactiveform.js
+++ js/interactiveform.js
@@ -15,3 +15,56 @@ $('#title').on('change', function(event) {
     hidePlaceHolder();
   }
 })
+
+// "Theme - JS Puns" display "Cornflower Blue," "Dark Slate Grey," and "Gold"
+let designColors = {
+  'js puns': [
+    {
+      'value': 'cornflowerblue',
+      'friendlyName': 'Cornflower Blue'
+    },
+    {
+      'value': 'darkslategrey',
+      'friendlyName': 'Dark Slate Gray'
+    },
+    {
+      'value': 'gold',
+      'friendlyName': 'Gold'
+    }
+  ],
+  // "Theme - Heart JS" display "Steel Blue," "Dim Grey," and "Tomato"
+  'heart js': [
+    {
+      'value': 'tomato',
+      'friendlyName': 'Tomato'
+    },
+    {
+      'value': 'steelblue',
+      'friendlyName': 'Steel Blue'
+    },
+    {
+      'value': 'dimgrey',
+      'friendlyName': 'Dim Grey'
+    }
+  ],
+};
+
+// if selected show specific colors
+$('#design').on('change', function(event) {
+  if(event.target.value==='js puns') {
+    let optionsHtml = ``;
+    let punsOptions = designColors['js puns'];
+    for (let i = 0; i < punsOptions.length; i++) {
+      optionsHtml += `<option value="${punsOptions[i].value}">${punsOptions[i].friendlyName}</option>`
+    }
+    $('#color').html(optionsHtml);
+  } else if (event.target.value==='heart js') {
+    let optionsHtml = ``;
+    let punsOptions = designColors['heart js'];
+    for (let i = 0; i < punsOptions.length; i++) {
+      optionsHtml += `<option value="${punsOptions[i].value}">${punsOptions[i].friendlyName}</option>`
+    }
+    $('#color').html(optionsHtml);
+
+    }
+  })
